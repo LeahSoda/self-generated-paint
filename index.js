9fa0bd18461bf2b2e22a0b65be16b51a6392e9b4
Julia Huang <staywinter0117@gmail.com>
2022-09-19 08:11:47 +0200

    scale up the start direction - a more explosive effect

--- index.js
+++ index.js
@@ -12,9 +12,14 @@ class Atom {
         //initialize the brush
         this.x = x;
         this.y = y;
-        this.radius = Math.random() * 8 + 2;
-        this.speedX = Math.random() * 8 - 4;    //x increment [-4,4]
-        this.speedY = Math.random() * 8 - 4;
+        //this.radius = Math.random()*1.5; //dotted lines
+        //this.speedX = Math.random() * 8 - 4;    //x increment [-4,4]
+      // this.speedY = Math.random() * 8 - 4;
+
+        //take 2
+        this.radius = Math.random()*4;
+        this.speedX=Math.random()*30-15;
+        this.speedY=Math.random()*30-15;
     }
     //update window?
     update() {
@@ -25,14 +30,15 @@ class Atom {
     draw() {
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        //maybe change color
         ctx.fill();
     }
 }
 
 //start autopaint whrn mouse is clicked
 canvas.addEventListener('click', function (e) {
-    //store the spots inside array
-        for (let i = 0; i < 20; i++) {
+    //the line grows in 20 start point
+        for (let i = 0; i < 300; i++) {
             atoms.push(new Atom(e.x, e.y));
     }
     console.log(e.x, e.y);
